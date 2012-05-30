6dc60b3d8400c8c87c7a3c7e5e062b4e351acbb1
Florian Thomas <flo@florianthomas.net>
2012-05-30 16:50:23 +0200

    Created empty Android Project with first Activity

---
+++ src/de/thm/ateam/memory/SelectUserActivity.java
@@ -0,0 +1,13 @@
+package de.thm.ateam.memory;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SelectUserActivity extends Activity {
+  /** Called when the activity is first created. */
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.main);
+  }
+}
