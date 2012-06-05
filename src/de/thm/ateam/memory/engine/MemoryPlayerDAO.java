6ca26838f34f0f7fb9e1d9f42a2a78c3b10c48ed
Frank Kevin Zey <frank.kevin.zey@mni.thm.de>
2012-06-05 13:32:08 +0200

    Database abstraction level ready v1, Player adjusted for DAL

--- src/de/thm/ateam/memory/engine/MemoryPlayerDAO.java
+++ src/de/thm/ateam/memory/engine/MemoryPlayerDAO.java
@@ -60,6 +60,16 @@ public class MemoryPlayerDAO extends PlayerDB implements PlayerDAO {
 	 * @see de.thm.ateam.memory.engine.interfaces.PlayerDAO#getPlayer(int)
 	 */
 	public Player getPlayer(int id) {
+		SQLiteDatabase db = sql.getReadableDatabase();
+		String[] projection = new String[] {
+			ID, NICK, WIN, LOSE, DRAW, HIT, SHOT
+		};
+		
+		Cursor c = db.query(TABLE_NAME, projection, ID, new String[]{String.valueOf(id)}, null, null, ID);
+		
+		if (c.moveToFirst())
+			return new Player(c);
+		
 		return null;
 	}
 
@@ -67,7 +77,70 @@ public class MemoryPlayerDAO extends PlayerDB implements PlayerDAO {
 	 * @see de.thm.ateam.memory.engine.interfaces.PlayerDAO#getPlayer(java.lang.String)
 	 */
 	public Player getPlayer(String nick) {
+		SQLiteDatabase db = sql.getReadableDatabase();
+		String[] projection = new String[] {
+			ID, NICK, WIN, LOSE, DRAW, HIT, SHOT
+		};
+		
+		Cursor c = db.query(TABLE_NAME, projection, NICK, new String[]{nick}, null, null, ID);
+		
+		if (c.moveToFirst())
+			return new Player(c);
+		
 		return null;
+	}
+
+	/* (non-Javadoc)
+	 * @see de.thm.ateam.memory.engine.interfaces.PlayerDAO#storePlayer(de.thm.ateam.memory.engine.type.Player)
+	 */
+	public boolean storePlayer(Player p) {
+		SQLiteDatabase db = sql.getWritableDatabase();
+		long r = db.insert(TABLE_NAME, null, this.createContentValues(p));
+		
+		if (r < 0)
+			return false;
+		
+		p.setID(r);
+		return true;
+	}
+
+	/* (non-Javadoc)
+	 * @see de.thm.ateam.memory.engine.interfaces.PlayerDAO#updatePlayer()
+	 */
+	public boolean updatePlayer(Player p) {
+		SQLiteDatabase db = sql.getWritableDatabase();
+		int r = db.update(TABLE_NAME, this.createContentValues(p), ID, new String[] {String.valueOf(p.getID())});
+		
+		if (r > 0)
+			return true;
+		else
+			return false;
+	}
+
+	/* (non-Javadoc)
+	 * @see de.thm.ateam.memory.engine.interfaces.PlayerDAO#removePlayer(de.thm.ateam.memory.engine.type.Player)
+	 */
+	public boolean removePlayer(Player p) {
+		SQLiteDatabase db = sql.getWritableDatabase();
+		int r = db.delete(TABLE_NAME, ID, new String[] {String.valueOf(p.getID())});
+		
+		if (r > 0)
+			return true;
+		else
+			return false;
+	}
+
+	/* (non-Javadoc)
+	 * @see de.thm.ateam.memory.engine.interfaces.PlayerDAO#removePlayer(int)
+	 */
+	public boolean removePlayer(int id) {
+		SQLiteDatabase db = sql.getWritableDatabase();
+		int r = db.delete(TABLE_NAME, ID, new String[] {String.valueOf(id)});
+		
+		if (r > 0)
+			return true;
+		else
+			return false;
 	}
 
 }
