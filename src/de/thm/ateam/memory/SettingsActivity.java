2cef08cff015882d09e715c2adf97567e6386f36
Florian Thomas <flo@florianthomas.net>
2012-07-27 00:05:56 +0200

    Added help menu

--- src/de/thm/ateam/memory/SettingsActivity.java
+++ src/de/thm/ateam/memory/SettingsActivity.java
@@ -2,6 +2,7 @@ package de.thm.ateam.memory;
 
 import android.app.ListActivity;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
@@ -14,7 +15,7 @@ import android.widget.ListView;
  */
 public class SettingsActivity extends ListActivity {
 	
-	private String[] settings = {"Load Deck", "Set Deck", "Remove Deck", "Memory Size"};
+	private String[] settings = {"Load Deck", "Set Deck", "Remove Deck", "Memory Size", "Help"};
 	
 	/**
 	 * 
@@ -65,6 +66,11 @@ public class SettingsActivity extends ListActivity {
 		case 3:
 			intent = new Intent(this, SelectMemorySizeActivity.class);
 			startActivity(intent);
+		case 4:
+		  String url = "http://homepages.thm.de/~fthm89/memory/index.html";
+		  intent = new Intent(Intent.ACTION_VIEW);
+		  intent.setData(Uri.parse(url));
+		  startActivity(intent);
 		default:
 			break;
 		}
