cc424dacfb1f25badb7340b22cfb0164a04aa49f
vanHagen <hagen.lauer@gmail.com>
2012-07-30 11:43:47 +0200

    fixes not existing bug ...

--- src/de/thm/ateam/memory/game/Game.java
+++ src/de/thm/ateam/memory/game/Game.java
@@ -1,4 +1,4 @@
- package de.thm.ateam.memory.game;
+package de.thm.ateam.memory.game;
 
 import java.util.ArrayList;
 
@@ -16,7 +16,7 @@ public class Game {
 	private ArrayList<Player> list;
 	private int current = -1;
 	Context ctx;
-	
+
 	/**
 	 * This is the Constructor, creates local PlayerList from attr.getPlayers()
 	 * 
@@ -25,21 +25,23 @@ public class Game {
 	 */
 	public Game(Context ctx, GameAttributes attr){
 		this.ctx = ctx;
-		this.list = attr.getPlayers();
-		this.attr = attr;
+		if(attr != null){
+			this.list = attr.getPlayers();
+			this.attr = attr;
+		}
 	}
-	
+
 	/**
 	 * Used to determine and return the next Player from the list
 	 * 
 	 * @return Player next
 	 */
-	
+
 	public Player next(){
 		++current;
 		return list.get(current%attr.getNumUsers());
 	}
-	
+
 	/**
 	 * Add Player, returns either success or failure
 	 * 
@@ -49,7 +51,7 @@ public class Game {
 	public int add(Player player){
 		return list.add(player)?list.indexOf(player):-1;
 	}
-	
+
 	/**
 	 * Get next Player via next() and notify the Player via myTurn(). Returns the current Player.
 	 * @return Player currentPlayer
@@ -60,38 +62,35 @@ public class Game {
 		Toast.makeText(ctx, temp.nick +", your turn!",Toast.LENGTH_SHORT).show();
 		return temp;
 	}
-	
-	
-	//TODO c'mon, return something. it isnt so hard.
-	
+
 	/**
 	 * Determines the winner of a match. Should be called at the end of a game
 	 */
 	public void getWinner(){
-	  int highscore  = 0;
-	  int numberOfWinners = 0;
-	  for(Player p : list){
-	    if(p.roundHits > highscore){
-	      highscore = p.roundHits;
-	      numberOfWinners = 1;
-	    }else if(p.roundHits == highscore){
-	      numberOfWinners++;
-	    }
-	  }
-	  // get all winners
-	  for(Player p : list){
-	    if(numberOfWinners == list.size()){
-	     p.roundDraw = true;
-	     continue;
-	    }
-	    if(p.roundHits == highscore){
-	      p.roundWin = true;
-	    }else{
-	      p.roundLose = true;
-	    }
-	  }
+		int highscore  = 0;
+		int numberOfWinners = 0;
+		for(Player p : list){
+			if(p.roundHits > highscore){
+				highscore = p.roundHits;
+				numberOfWinners = 1;
+			}else if(p.roundHits == highscore){
+				numberOfWinners++;
+			}
+		}
+		// get all winners
+		for(Player p : list){
+			if(numberOfWinners == list.size()){
+				p.roundDraw = true;
+				continue;
+			}
+			if(p.roundHits == highscore){
+				p.roundWin = true;
+			}else{
+				p.roundLose = true;
+			}
+		}
 	}
-	
+
 	/**
 	 * 
 	 * This Method is used to initialize the View of the Game Activity, should be overridden and return a View or descendant of View.
@@ -102,14 +101,14 @@ public class Game {
 		tv.setText("You are using the method of "+this.getClass().getSimpleName()+" and you might want to override this.");
 		return tv;
 	}
-	
+
 	/**
 	 * To be  overridden, if required.
 	 */
 	public void newGame(){
 		Log.i(TAG, "new Game()");
 	}
-	
+
 	/**
 	 * This Method is used to controll how the game ends.
 	 */
